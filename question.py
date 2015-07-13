0ffe86a8d6e89c492725092b9b2a17c8ed0a7dd2
bzitzman <bzitzman@answersingenesis.org>
2015-07-13 00:27:10 -0400

    fixed incorrec answers reporting

--- question.py
+++ question.py
@@ -34,6 +34,8 @@ def results(result, question):
         print "CORRECT"
         lookup.search_initiate(**{"search_type" : "annagram", "srch_trm": question})
     elif result == 'incorrect':
+        kwargs = {'result':result, 'question':question} 
+        update_db(**kwargs)
         print "INCORRECT"
         lookup.search_initiate(**{"search_type" : "annagram", "srch_trm": question})
 
@@ -41,10 +43,15 @@ def update_db(**kwargs):
     current_time = calendar.timegm(time.gmtime())    
     question = kwargs['question']
     if kwargs['result'] == 'correct':
-       command1 = "INSERT OR IGNORE INTO study_list (question,correct_answers,incorrect_answers,last_asked,streak,last_correct,last_incorrect,length) VALUES('%s',0,0,0,0,0,0,%d)" % ( question,len(question)) 
-       command2 = "UPDATE study_list SET correct_answers = correct_answers +1, last_correct = %d, last_asked = %d,streak = streak +1 WHERE question = '%s'" % (current_time, current_time, question)
-    leave_calc.update_db(command1)
-    leave_calc.update_db(command2)
+        command1 = "INSERT OR IGNORE INTO study_list (question,correct_answers,incorrect_answers,last_asked,streak,last_correct,last_incorrect,length) VALUES('%s',0,0,0,0,0,0,%d)" % ( question,len(question)) 
+        command2 = "UPDATE study_list SET correct_answers = correct_answers +1, last_correct = %d, last_asked = %d,streak = streak +1 WHERE question = '%s'" % (current_time, current_time, question)
+        leave_calc.update_db(command1)
+        leave_calc.update_db(command2)
+    if kwargs['result'] == 'incorrect':
+        command1 = "INSERT OR IGNORE INTO study_list (question,correct_answers,incorrect_answers,last_asked,streak,last_correct,last_incorrect,length) VALUES('%s',0,0,0,0,0,0,%d)" % ( question,len(question)) 
+        command2 = "UPDATE study_list SET incorrect_answers = incorrect_answers +1, last_incorrect = %d, last_asked = %d,streak = 0 WHERE question = '%s'" % (current_time, current_time, question)
+        leave_calc.update_db(command1)
+        leave_calc.update_db(command2)
 
 
   
