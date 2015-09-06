5f878c1efe753b6163310240a3fee2c02b448c25
Brandon Zitzman <bzitzman@answersingenesis.org>
2015-09-05 21:19:59 -0400

    updates to quiz.py

--- quiz.py
+++ quiz.py
@@ -1,6 +1,28 @@
 class Quiz(object):
     def __init__(self, questions):
+        self.count = 1
         self.questions = questions
-        self.correct = 0
+        self.correct = 20
         self.incorrect = 0
         self.questions_correct = []
+        self.questions_incorrect = []
+        self.total = self.correct + self.incorrect
+
+    def quiz_main(self):
+        print self.questions
+        for item in self.questions:
+            self.ask_question(item)
+            self.update
+
+    def ask_question(self, question):
+        self.quiz_status()
+
+    def quiz_status(self):
+        print "Question %d of %d" % (self.count, len(self.questions))
+        if self.correct + self.incorrect != 0:
+            print "%d of %d Correct %.2f%% " % (self.correct, self.total, 100*float(self.correct)/float(self.total))
+
+test = Quiz(['help','me','crazy'])
+
+test.quiz_main()
+
